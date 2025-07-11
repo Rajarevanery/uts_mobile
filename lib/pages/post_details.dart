fb785d525bd4f5a617c28145f25f4e0063eefb47
Raja Revanery Ramadhan <rajarevaneri@gmail.com>
2025-07-11 14:43:33 +0700

    polished: finish

--- lib/pages/post_details.dart
+++ lib/pages/post_details.dart
@@ -73,12 +73,10 @@ class _PostDetailsState extends State<PostDetails> {
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
-              padding: const EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
                     child: Stack(
                       children: [
                         Image.network(
@@ -103,40 +101,50 @@ class _PostDetailsState extends State<PostDetails> {
                   ),
 
                   const SizedBox(height: 16),
-                  Text(
-                    post!.title.toString().replaceAll('\n', ' '),
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                  
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          post!.title.toString().replaceAll('\n', ' '),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          post!.body.toString().replaceAll('\n', ' '),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white70,
+                            height: 1.4,
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        TextButton.icon(
+                          onPressed: fetchComments,
+                          icon: const Icon(Icons.comment, color: Colors.white),
+                          label: const Text(
+                            "View Comments",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xFF2D3A45),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 12,
+                            ),
+                          ),
+                        ),
+                        
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 12),
-                  Text(
-                    post!.body.toString().replaceAll('\n', ' '),
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white70,
-                      height: 1.4,
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  TextButton.icon(
-                    onPressed: fetchComments,
-                    icon: const Icon(Icons.comment, color: Colors.white),
-                    label: const Text(
-                      "View Comments",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: const Color(0xFF2D3A45),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 12,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 24),
 
                   if (isCommentsLoading)
                     const Padding(
@@ -144,78 +152,81 @@ class _PostDetailsState extends State<PostDetails> {
                       child: Center(child: CircularProgressIndicator()),
                     ),
                   if (showComments)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ...comments.take(3).map((comment) {
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 16),
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: const Color(0xFF1A1A1C),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor: Colors.blueGrey,
-                                  child: Text(
-                                    comment.email.substring(0, 1).toUpperCase(),
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ...comments.take(3).map((comment) {
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 16),
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF1A1A1C),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    backgroundColor: Colors.blueGrey,
+                                    child: Text(
+                                      comment.email.substring(0, 1).toUpperCase(),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const SizedBox(width: 12),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        comment.email,
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
+                                  const SizedBox(width: 12),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          comment.email,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        comment.body.toString().replaceAll(
-                                          '\n',
-                                          ' ',
+                                        const SizedBox(height: 8),
+                                        Text(
+                                          comment.body.toString().replaceAll(
+                                            '\n',
+                                            ' ',
+                                          ),
+                                          style: const TextStyle(
+                                            color: Colors.white70,
+                                            height: 1.4,
+                                          ),
                                         ),
-                                        style: const TextStyle(
-                                          color: Colors.white70,
-                                          height: 1.4,
-                                        ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
+                                ],
+                              ),
+                            );
+                          }),
+                          const SizedBox(height: 8),
+                          GestureDetector(
+                            onTap: () {
+                              context.push('/post/${widget.postId}/comments');
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 12),
+                              child: Text(
+                                "View All Comments",
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  decoration: TextDecoration.underline,
                                 ),
-                              ],
-                            ),
-                          );
-                        }),
-                        const SizedBox(height: 8),
-                        GestureDetector(
-                          onTap: () {
-                            context.push('/post/${widget.postId}/comments');
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 12),
-                            child: Text(
-                              "View All Comments",
-                              style: TextStyle(
-                                color: Colors.white70,
-                                decoration: TextDecoration.underline,
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                 ],
               ),
